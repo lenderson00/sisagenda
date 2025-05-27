7c9505c8d23d9785af9a52b0547552bd67c3a41b
Lenderson Macedo <github@lenderson.com.br>
2025-05-27 19:07:26 -0300

    feat: implement user detail page with authentication and data fetching
    - Created a new UserPage component to display individual user details, including name and email.
    - Integrated authentication checks to ensure only authorized users can access the page.
    - Added data fetching logic using Prisma to retrieve user information based on the provided user ID.
    - Implemented redirection for unauthorized access and handling of non-existent users with a notFound response.

--- src/app/admin/usuarios/[user]/page.tsx
+++ src/app/admin/usuarios/[user]/page.tsx
@@ -1,5 +1,55 @@
-const UserPage = () => {
-	return <div>UserPage</div>;
+import { BreadcrumbNav } from "@/components/breadcrumb-nav";
+import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { notFound, redirect } from "next/navigation";
+
+const UserPage = async ({ params }: { params: Promise<{ user: string }> }) => {
+  const session = await auth();
+  const { user } = await params;
+
+  if (!session) {
+    redirect("/");
+  }
+
+  const orgId = session.user.organizationId;
+
+  if (!orgId) {
+    redirect("/");
+  }
+
+  const userResult = await prisma.user.findUnique({
+    where: {
+      id: user,
+    },
+  });
+
+  if (!userResult) {
+    notFound();
+  }
+
+  const breadcrumbItems = [
+    { label: "Dashboard", href: "/" },
+    { label: "Usuários", href: "/usuarios" },
+    { label: userResult.name ?? "", current: true },
+  ];
+  return (
+    <>
+      {" "}
+      <div className="border-b ">
+        <div className="container mx-auto px-6 py-8">
+          <BreadcrumbNav items={breadcrumbItems} />
+          <div className="flex items-center justify-between mt-4">
+            <div>
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                {userResult.name}
+              </h1>
+              <p className="text-gray-600">{userResult.email}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserPage;
