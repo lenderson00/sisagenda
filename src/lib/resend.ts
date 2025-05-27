812aeb8997d4c7bc887981a2b3dba958ecfda3f8
Lenderson Macedo <github@lenderson.com.br>
2025-05-26 22:11:58 -0300

    fix: update email sender address for password reset notifications
    - Changed the sender email address in the sendEmail function to "SisAgenda <reset@email.pxel.com.br>" for improved branding in password reset communications.

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -5,7 +5,7 @@ export const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async (to: string, subject: string, reactEmail: React.ReactNode) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "SisAgenda <reset@email.pxel.com.br>",
       to,
       subject,
       react: reactEmail,
