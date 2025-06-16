ab685fa70a7af8cb27226925fc8987b632a5a3ec
Lenderson Macedo <github@lenderson.com.brn>
2025-06-16 17:31:16 -0300

    feat: integrate SiteHeader into FornecedorLayout
    - Replaced TopBar with SiteHeader for improved layout structure.
    - Updated layout to enhance user experience and maintain consistency across components.

--- src/app/fornecedor/(main)/layout.tsx
+++ src/app/fornecedor/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import Footer from "@/components/footer";
+import { SiteHeader } from "@/components/site-header";
 import { TopBar } from "@/components/top-bar";
 import { TopNavigation } from "@/components/top-navigation";
 
@@ -10,7 +11,7 @@ const tabs = [
 const FornecedorLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col">
-      <TopBar className="border-b" />
+      <SiteHeader />
       <div className="min-h-[60vh] bg-neutral-50 dark:bg-neutral-900 px-4 ">
         {children}
       </div>
