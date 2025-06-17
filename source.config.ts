03767632c6412e2a91bad17634383deb0cd4f20d
Lenderson Macedo <github@lenderson.com.brn>
2025-06-17 09:06:14 -0300

    refactor: simplify role schema in source.config.ts
    - Replaced the inline role enum definition with a constant for improved readability and maintainability.
    - Updated the role field in the frontmatter schema to use the new roles constant, streamlining the code structure.

--- source.config.ts
+++ source.config.ts
@@ -1,22 +1,20 @@
 import { defineDocs, frontmatterSchema } from "fumadocs-mdx/config";
 import { z } from "zod";
 
+const roles = z.enum([
+  "SUPER_ADMIN",
+  "ADMIN",
+  "COMIMSUP_ADMIN",
+  "COMRJ_ADMIN",
+  "USER",
+  "FORNECEDOR",
+]);
+
 export const docs = defineDocs({
   dir: "content/docs",
   docs: {
     schema: frontmatterSchema.extend({
-      role: z
-        .array(
-          z.enum([
-            "SUPER_ADMIN",
-            "ADMIN",
-            "COMIMSUP_ADMIN",
-            "COMRJ_ADMIN",
-            "USER",
-            "FORNECEDOR",
-          ]),
-        )
-        .optional(),
+      role: z.array(roles).optional(),
     }),
   },
 });
