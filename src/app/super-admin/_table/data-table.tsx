0c7963683b0e03478eb6c3fa751b54373aa2d8cd
Lenderson Macedo <github@lenderson.com.br>
2025-06-26 21:42:45 -0300

    feat: refactor super admin layout and enhance organization management UI
    - Updated the SuperAdminLayout to integrate a sidebar and improve overall structure with a new SuperAdminSidebar component.
    - Enhanced the OrganizationList component to include loading states and improved data handling with a new OrganizationsDataTable.
    - Refactored the columns configuration for the organization data table, introducing a more modular approach with tst-columns.
    - Added navigation items for better organization management in the super admin interface.
    - Ensured all changes adhere to clean code principles and improve user experience.

--- src/app/super-admin/_table/data-table.tsx
+++ src/app/super-admin/_table/data-table.tsx
@@ -1,185 +1,74 @@
 "use client";
 
+import { DataTableFilter, useDataTableFilters } from "@/components/data-table";
+import { DataTable } from "@/components/data-table";
 import {
-  type ColumnDef,
-  type ColumnFiltersState,
-  type SortingState,
-  type VisibilityState,
-  flexRender,
+  createTSTColumns,
+  createTSTFilters,
+} from "@/components/data-table/integrations/tanstack-table";
+import type { Organization } from "@prisma/client";
+import {
   getCoreRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
-  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+import { useMemo } from "react";
+import { columnsConfig } from "./columns";
+import { tstColumnsDefs } from "./tst-columns";
 
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { ChevronDown } from "lucide-react";
-import { useState } from "react";
+type OrganizationResponse = Organization & {
+  comimsup: {
+    name: string;
+  };
+};
 
-interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[];
-  data: TData[];
+interface OrganizationsDataTableProps {
+  data: OrganizationResponse[];
 }
 
-export function DataTable<TData, TValue>({
-  columns,
-  data,
-}: DataTableProps<TData, TValue>) {
-  const [sorting, setSorting] = useState<SortingState>([]);
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
-  const [rowSelection, setRowSelection] = useState({});
+export function OrganizationsDataTable({ data }: OrganizationsDataTableProps) {
+  const { columns, filters, actions, strategy } = useDataTableFilters({
+    strategy: "client",
+    data,
+    columnsConfig,
+  });
+
+  const tstColumns = useMemo(
+    () =>
+      createTSTColumns({
+        columns: tstColumnsDefs,
+        configs: columns,
+      }),
+    [columns],
+  );
+
+  const tstFilters = useMemo(() => createTSTFilters(filters), [filters]);
 
   const table = useReactTable({
-    data,
-    columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
+    data: data,
+    columns: tstColumns,
+    getRowId: (row) => row.id.toString(),
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
     state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-      rowSelection,
+      columnFilters: tstFilters,
     },
   });
 
   return (
     <div className="w-full">
-      <div className="flex items-center py-4">
-        <Input
-          placeholder="Filtrar OM..."
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("name")?.setFilterValue(event.target.value)
-          }
-          className="w-full md:max-w-sm bg-background"
+      <div className="flex items-center pb-4 gap-2">
+        <DataTableFilter
+          filters={filters}
+          columns={columns}
+          actions={actions}
+          strategy={strategy}
+          locale="pt"
         />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild className="hidden md:block">
-            <Button
-              variant="outline"
-              className="ml-auto min-w-24 flex items-center gap-2"
-            >
-              Colunas
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter((column) => column.getCanHide())
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
-                  >
-                    {column.id}
-                  </DropdownMenuCheckboxItem>
-                );
-              })}
-          </DropdownMenuContent>
-        </DropdownMenu>
       </div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext(),
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext(),
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  Nenhum resultado encontrado.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="text-muted-foreground flex-1 text-sm">
-          {table.getFilteredSelectedRowModel().rows.length} de{" "}
-          {table.getFilteredRowModel().rows.length} linhas selecionadas.
-        </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Anterior
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Próximo
-          </Button>
-        </div>
-      </div>
+      <DataTable table={table} actions={actions} />
     </div>
   );
 }
---
+++ src/app/super-admin/_table/tst-columns.tsx
@@ -0,0 +1,133 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import dayjs from "@/lib/dayjs";
+import type { Organization } from "@prisma/client";
+import { IconDots, IconEye, IconPencil } from "@tabler/icons-react";
+import { createColumnHelper } from "@tanstack/react-table";
+import Link from "next/link";
+
+type OrganizationResponse = Organization & {
+  comimsup: {
+    name: string;
+  };
+};
+
+const columnHelper = createColumnHelper<OrganizationResponse>();
+
+export const tstColumnsDefs = [
+  // Name Column
+  columnHelper.accessor((row) => row.name, {
+    id: "name",
+    header: "Nome",
+    cell: ({ row }) => {
+      return (
+        <Link href={`/${row.original.id}`}>
+          <div className="text-sm min-w-[190px] truncate">
+            {row.original.name}
+          </div>
+        </Link>
+      );
+    },
+  }),
+
+  // Sigla Column
+  columnHelper.accessor((row) => row.sigla, {
+    id: "sigla",
+    header: "Sigla",
+    cell: ({ row }) => {
+      return (
+        <div className="text-sm text-muted-foreground w-[100px]">
+          {row.original.sigla}
+        </div>
+      );
+    },
+  }),
+
+  // COMIMSUP Column
+  columnHelper.accessor((row) => row.comimsup?.name, {
+    id: "comimsup",
+    header: "COMIMSUP",
+    cell: ({ row }) => {
+      if (row.original?.comimsup?.name) {
+        return (
+          <div className="text-sm w-[300px] truncate ">
+            {row.original.comimsup.name}
+          </div>
+        );
+      }
+      return null;
+    },
+  }),
+
+  // Role Column
+  columnHelper.accessor((row) => row.role, {
+    id: "role",
+    header: "Papel",
+    cell: ({ row }) => {
+      const name =
+        row.original.role === "DEPOSITO" ? "Depósito" : row.original.role;
+
+      return (
+        <Badge
+          variant="outline"
+          className="flex gap-1 px-1.5 text-muted-foreground [&_svg]:size-3 w-fit"
+        >
+          {name}
+        </Badge>
+      );
+    },
+  }),
+
+  // Created At Column
+  columnHelper.accessor((row) => new Date(row.createdAt), {
+    id: "createdAt",
+    header: "Criado em",
+    cell: ({ row }) => {
+      const date = dayjs(row.original.createdAt).format(
+        "D [de] MMMM [de] YYYY",
+      );
+
+      return <div className="text-sm w-[100px]">{date}</div>;
+    },
+  }),
+
+  // Actions Column
+  columnHelper.display({
+    id: "actions",
+    header: "",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              className="flex size-8 text-muted-foreground data-[state=open]:bg-muted"
+              size="icon"
+            >
+              <IconDots />
+              <span className="sr-only">Open menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="w-48">
+            <DropdownMenuItem asChild>
+              <Link href={`/${row.original.id}`}>Ver Detalhes</Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <Link href={`/${row.original.id}`}>Editar</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  }),
+];
